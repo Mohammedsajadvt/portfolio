8aee5a8df40ee4721ccb4a13e915648aba1cbd3d
Mohammed Sajad VT <mohammedsajadvt@gmail.com>
2025-05-08 23:32:27 +0530

    Added learn more and github functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("stylish").addEventListener("click", function() {
+    window.open("https://github.com/Mohammedsajadvt/stylish", "_blank");
+  });
+
+  document.getElementById("netflix").addEventListener("click", function() {
+    window.open("https://github.com/Mohammedsajadvt/Netflix-Clone", "_blank");
+  });
